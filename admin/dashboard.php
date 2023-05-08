99eba85720de25997712c101b4efd953bbcf5635
Pratham Manjhi <prathammanjhi2@gmail.com>
2023-05-09 00:58:26 +0530

    ajax

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -16,9 +16,43 @@ adminLogin();
 
 <body class="bg-light">
 
-    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
-        <h3 class="mb-0">ADMIN PANEL</h3>
+    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
+        <h3 class="mb-0 h-font">HOTEL</h3>
         <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
+    </div>
+
+    <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
+
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid flex-lg-column align-items-stretch ">
+                <H4 class="mt-4 text-light">ADMIN PANEL</H4>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon shadow-none"></span>
+                </button>
+                <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
+
+                    <!-- nav pill -->
+                    <ul class="nav nav-pills flex-column">
+
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Rooms</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Settings</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
     </div>
 
     <?php require('include/script.php') ?>
